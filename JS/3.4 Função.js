1dd3c903c4c3fa3478be28927ec5e0ce8acc3360
GuilhermeProença <127969945+GuilhermeProencaCorrea@users.noreply.github.com>
2023-08-17 23:24:34 -0300

    Atualizações

--- JS/3.4 Função.js
+++ JS/3.4 Função.js
@@ -1,8 +1,20 @@
 let nun = [1,2,3,4];
 
+/*
+É um método em JavaScript que permite iterar sobre um array e executar
+uma função para cada elemento no array. Ele recebe como argumento uma 
+função de retorno de chamada que define o que fazer com cada elemento. 
+A função de retorno de chamada pode receber até três argumentos: o elemento
+atual sendo processado, seu índice e o array sendo percorrido.
+*/
+
 // Função Arrow é uma função sem nome, usada em casos especificos
-// Espressão lambda nun => (Percorre toda array)
+// Espressão lambda nun => (Percorre toda array) É uma função anônima que pode ser usada em várias linguagens de programação.
 nun.forEach(nun => console.log(nun));
+
+nun.forEach((nun) =>{
+     console.log(nun+2)
+});
 
 
 
